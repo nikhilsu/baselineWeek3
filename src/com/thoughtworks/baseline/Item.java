d50b9e430fc29b4741285874ccdbff05911a97e6
nikhilsu <nikhilsu@thoughtworks.com>
2015-09-10 12:10:39 +0530

    Added Spec to Item Class to return the Net cost of the item as 110 when its gross Cost was 100 and its tax percentage is 10

--- src/com/thoughtworks/baseline/Item.java
+++ src/com/thoughtworks/baseline/Item.java
@@ -3,13 +3,17 @@ package com.thoughtworks.baseline;
 public class Item {
     private String nameOfTheItem;
     private double grossCost;
+    private double taxPercentageOnTheItem;
 
-    public Item(String nameOfTheItem, double grossCost) {
+    public Item(String nameOfTheItem, double grossCost, double taxPercentageOnTheItem) {
         this.nameOfTheItem = nameOfTheItem;
         this.grossCost = grossCost;
+        this.taxPercentageOnTheItem = taxPercentageOnTheItem;
     }
 
     public double computeNetCost() {
-        return 100;
+        double taxOnItem = this.grossCost * this.taxPercentageOnTheItem / 100;
+        double netAmount = taxOnItem + this.grossCost;
+        return netAmount;
     }
 }
--- test/com/thoughtworks/baseline/ItemTest.java
+++ test/com/thoughtworks/baseline/ItemTest.java
@@ -8,9 +8,15 @@ public class ItemTest {
 
     @Test
     public void shouldReturnTheNetAmountOfTheItemAsTheGrossAmountWhenTheTaxIsZero() {
-        Item item = new Item("Book", 100);
+        Item item = new Item("Book", 100, 0);
 
         assertEquals(100, item.computeNetCost(), 0.0);
     }
 
+    @Test
+    public void shouldReturnTheNetAmountOfTheItemAs110WhenTheGrossAmountis100AndTax10Percent() {
+        Item item = new Item("Chocolate", 100, 10);
+
+        assertEquals(110, item.computeNetCost(), 0.0);
+    }
 }
